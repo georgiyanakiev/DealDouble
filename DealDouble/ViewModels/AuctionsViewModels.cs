3d0234e708bced1c079d265103bb8ac1672a5115
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-05-26 20:57:04 +0100

    added linq to save images

--- DealDouble/ViewModels/AuctionsViewModels.cs
+++ DealDouble/ViewModels/AuctionsViewModels.cs
@@ -16,4 +16,21 @@ namespace DealDouble.ViewModels
         public List<Auction> AllAuctions { get; set; }
         public List<Auction> PromotedAuctions { get; set; }
     }
+
+    public class AuctionsDetailsViewModel : PageViewModel
+    {
+
+        public Auction Auctions { get; set; }
+    }
+
+    public class CreateAuctionViewModel : PageViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal ActualAmount { get; set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndingTime { get; set; }
+        public string AuctionPictures { get; set; }
+
+    }
 }
