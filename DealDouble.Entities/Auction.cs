81af270e5141f517a942b375649e094597eea887
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-06-06 23:14:52 +0100

    added images in gallery for auctions

--- DealDouble.Entities/Auction.cs
+++ DealDouble.Entities/Auction.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 namespace DealDouble.Entities
 {
     public class Auction : BaseEntity
-    {
-        
+    {   //foreign key
+        public virtual Category Category { get; set; }
+        public int CategoryID { get; set; }
+
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal ActualAmount { get; set; }
         public DateTime StartingTime { get; set; }
         public DateTime EndingTime { get; set; }
-        public List<AuctionPicture> AuctionPictures { get; set; }
+        public virtual List<AuctionPicture> AuctionPictures { get; set; }
     }
 }
